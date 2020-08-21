1aaf05a67693b1eb985ba577889549e107255e50
DeLayne LaBove <delayne.labove@gmail.com>
2020-08-21 10:36:31 -0500

    Testing count

--- sick-fits/frontend/pages/index.js
+++ sick-fits/frontend/pages/index.js
@@ -4,7 +4,8 @@ const Home = props => (
     <div>
         <p>Hey!</p>
         <p>Testing push count!</p>
-        <p>Testing one more push count!</p>
+        <p>Testing another push count!</p>
+
     </div>
 )
 
