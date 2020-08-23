5fe543d5c75f04dbd7706666787c1a67f30f4b90
DeLayne LaBove <delayne.labove@gmail.com>
2020-08-23 14:31:26 -0500

    added sell page

--- sick-fits/frontend/pages/index.js
+++ sick-fits/frontend/pages/index.js
@@ -1,11 +1,8 @@
-import React from 'react';
 
 const Home = props => (
     <div>
         <p>Hey!</p>
-        <p>Testing push count!</p>
-        <p>Testing another push count!</p>
-
+        <a href="/sell"></a>
     </div>
 )
 
