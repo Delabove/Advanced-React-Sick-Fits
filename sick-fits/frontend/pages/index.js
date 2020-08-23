2adf093626a1e052d13c4b557d4d20a4c0b26e07
DeLayne LaBove <delayne.labove@gmail.com>
2020-08-23 15:12:48 -0500

    linked sell and home page with next link component

--- sick-fits/frontend/pages/index.js
+++ sick-fits/frontend/pages/index.js
@@ -1,9 +1,14 @@
+import Link from 'next/link';
 
 const Home = props => (
-    <div>
-        <p>Hey!</p>
-        <a href="/sell"></a>
-    </div>
+<div>
+
+    <p>Hey!</p>
+    <Link href="/sell">
+        <a >Sell!</a>
+    </Link>
+
+</div>
 )
 
 export default Home;
