2f77882a5c4adb7c8d60ceebe41bd7ff58b8eb20
DeLayne LaBove <delayne.labove@gmail.com>
2020-08-24 13:52:58 -0500

    background and text style, content restraints

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,16 +1,36 @@
 import React, { Component } from 'react'
 import Header from '../components/Header';
 import Meta from '../components/Meta';
+import styled from 'styled-components';
+
+const StyledPage = styled.div`
+    background: white;
+    color: black;
+`;
+
+//following code creates restraints for our
+// product grid so it does not span across the
+// whole page
+const Inner = styled.div `
+    max-width: 1000px;
+    margin: 0 auto;
+    padding: 2rem;
+`;
 
 class Page extends Component {
     render() {
         return (
-            <div>
+            <StyledPage>
                 <Meta />
                 <Header />
-                {this.props.children}
-            </div>
+                <Inner>
+                    {this.props.children}
+                </Inner>
+            </StyledPage>
         );
     }
 }
 export default Page;
+
+
+
