34c439321eb05b8104d37143da7da58b1b455b1c
DeLayne LaBove <delayne.labove@gmail.com>
2020-08-31 06:46:58 -0500

    Imperatively routing

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,8 +1,9 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import styled, {ThemeProvider, injectGlobal} from 'styled-components';
 import Header from '../components/Header';
 import Meta from '../components/Meta';
-import styled, {ThemeProvider, injectGlobal} from 'styled-components';
 
+//only components inside themeProvider can access theme
 const theme = {
     red: '#FF0000',
     black: '#393939',
@@ -27,6 +28,38 @@ const Inner = styled.div `
     margin: 0 auto;
     padding: 2rem;
 `;
+
+// bc this is global css you must add selector
+// ie html
+injectGlobal`
+    @font-face {
+        font-family: 'radnika_next';
+        src: url('/static/radnikanext-medium-webfont.woff2')
+        format('woff2');
+        font-weight:normal;
+        font-style: normal;
+    }
+    html{
+        box-sizing: border-box;
+        font-size: 10px; /* base px */
+    }
+    *, *:before, *:after{
+        box-sizing: inherit;
+    }
+    body{
+        padding: 0;
+        margin: 0;
+        font-size: 1.5rem; /* 15 px */
+        line-height: 2;
+        font-family: 'radnika_next';
+    }
+    a{
+        text-decoration: none;
+        color: ${theme.black};
+    }
+
+`;
+
 
 class Page extends Component {
     render() {
